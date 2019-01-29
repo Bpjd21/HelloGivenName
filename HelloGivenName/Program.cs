24d45acb0a9336a83140a9c39515b354e5638ff4
Bpjd21 <bpjd21@yahoo.com>
2019-01-28 22:54:48 -0500

    Working Code

---
+++ HelloGivenName/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HelloGivenName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a name: ");
+            SayHello(Console.ReadLine());
+
+            Console.Write("Enter another name: ");
+            SayHello(Console.ReadLine());
+
+            Console.Write("Enter a thrid name: ");
+            SayHello(Console.ReadLine(), true);
+
+            Console.ReadKey();
+        }
+
+        static void SayHello(string name, bool askHowAreYou = false)
+        {
+            Console.Write("Hello " + name + "!");
+
+            if (askHowAreYou) 
+            Console.Write(" How are you?");
+            else
+                Console.WriteLine();
+            Console.WriteLine();
+               
+        }
+
+    }
+}
